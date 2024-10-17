5be2faaa2b2c3644144df48aabbc36caa10ff99d
Rahmat Arayyan <rahmatarayyan26@gmail.com>
2024-10-17 14:02:32 +0700

    displays data for all users

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,48 +1,47 @@
 <!-- src/components/DataTable.vue -->
 <template>
-    <div>
-      <table class="min-w-full bg-white">
-        <thead>
-          <tr>
-            <th class="py-2 px-4 border-b">NIK</th>
-            <th class="py-2 px-4 border-b">Nama</th>
-            <th class="py-2 px-4 border-b">Email</th>
-            <th class="py-2 px-4 border-b">Tanggal Daftar</th>
-            <th class="py-2 px-4 border-b">Tanggal Menabung</th>
-            <th class="py-2 px-4 border-b">Jumlah Setoran</th>
-            <th class="py-2 px-4 border-b">Nama Petugas</th>
-            <th class="py-2 px-4 border-b">Saldo</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in items" :key="item.id">
-            <td class="py-2 px-4 border-b">{{ item.id }}</td>
-            <td class="py-2 px-4 border-b">{{ item.name }}</td>
-            <td class="py-2 px-4 border-b">{{ item.email }}</td>
-            <td class="py-2 px-4 border-b">{{ item.registeredAt }}</td>
-            <td class="py-2 px-4 border-b">{{ item.totalDeposits }}</td>
-            <td class="py-2 px-4 border-b">{{ item.totalWithdrawals }}</td>
-            <td class="py-2 px-4 border-b">{{ item.balance }}</td>
-            <td class="py-2 px-4 border-b">{{ item.status }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'DataTable',
-    props: {
-      items: {
-        type: Array,
-        required: true,
-      },
-    },
+  <div>
+    <table class="min-w-full bg-white">
+      <thead>
+        <tr>
+          <th class="py-2 px-4 border-b">NIK</th>
+          <th class="py-2 px-4 border-b">Nama</th>
+          <th class="py-2 px-4 border-b">Email</th>
+          <th class="py-2 px-4 border-b">Tanggal Daftar</th>
+          <th class="py-2 px-4 border-b">Tanggal Menabung</th>
+          <th class="py-2 px-4 border-b">Jumlah Setoran</th>
+          <th class="py-2 px-4 border-b">Nama Petugas</th>
+          <th class="py-2 px-4 border-b">Saldo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td class="py-2 px-4 border-b">{{ item.id }}</td>
+          <td class="py-2 px-4 border-b">{{ item.name }}</td>
+          <td class="py-2 px-4 border-b">{{ item.email }}</td>
+          <td class="py-2 px-4 border-b">{{ item.registeredAt }}</td>
+          <td class="py-2 px-4 border-b">{{ item.totalDeposits }}</td>
+          <td class="py-2 px-4 border-b">{{ item.totalWithdrawals }}</td>
+          <td class="py-2 px-4 border-b">{{ item.balance }}</td>
+          <td class="py-2 px-4 border-b">{{ item.status }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
   }
-  </script>
-  
-  <style scoped>
-  /* Tambahan gaya jika diperlukan */
-  </style>
-  
+}
+</script>
+
+<style scoped>
+/* Tambahan gaya jika diperlukan */
+</style>
--- src/components/DataTableUser.vue
+++ src/components/DataTableUser.vue
@@ -1,48 +1,41 @@
 <!-- src/components/DataTableUset.vue -->
 <template>
-    <div>
-      <table class="min-w-full bg-white">
-        <thead>
-          <tr>
-            <th class="py-2 px-4 border-b">NIK</th>
-            <th class="py-2 px-4 border-b">Nama Lengkap</th>
-            <th class="py-2 px-4 border-b">Alamat</th>
-            <th class="py-2 px-4 border-b">Tujuan Menabung</th>
-            <th class="py-2 px-4 border-b">Total Setoran</th>
-            <th class="py-2 px-4 border-b">Lama Menabung</th>
-            <th class="py-2 px-4 border-b">Saldo</th>
-            <th class="py-2 px-4 border-b">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in items" :key="item.id">
-            <td class="py-2 px-4 border-b">{{ item.id }}</td>
-            <td class="py-2 px-4 border-b">{{ item.name }}</td>
-            <td class="py-2 px-4 border-b">{{ item.email }}</td>
-            <td class="py-2 px-4 border-b">{{ item.registeredAt }}</td>
-            <td class="py-2 px-4 border-b">{{ item.totalDeposits }}</td>
-            <td class="py-2 px-4 border-b">{{ item.totalWithdrawals }}</td>
-            <td class="py-2 px-4 border-b">{{ item.balance }}</td>
-            <td class="py-2 px-4 border-b">{{ item.status }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'DataTableUser',
-    props: {
-      items: {
-        type: Array,
-        required: true,
-      },
-    },
+  <div>
+    <table class="min-w-full bg-white">
+      <thead>
+        <tr>
+          <th class="py-2 px-4 border-b">User ID</th>
+          <th class="py-2 px-4 border-b">Username</th>
+          <th class="py-2 px-4 border-b">Nama Lengkap</th>
+          <th class="py-2 px-4 border-b">Email</th>
+          <th class="py-2 px-4 border-b">Password</th>
+          <th class="py-2 px-4 border-b">No.Telepon</th>
+          <th class="py-2 px-4 border-b">Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.user_id">
+          <td class="py-2 px-4 border-b">{{ item.user_id }}</td>
+          <td class="py-2 px-4 border-b">{{ item.username }}</td>
+          <td class="py-2 px-4 border-b">{{ item.full_name }}</td>
+          <td class="py-2 px-4 border-b">{{ item.email }}</td>
+          <td class="py-2 px-4 border-b">{{ item.password }}</td>
+          <td class="py-2 px-4 border-b">{{ item.phone_number }}</td>
+          <td class="py-2 px-4 border-b">{{ item.role }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTableUser',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
   }
-  </script>
-  
-  <style scoped>
-  /* Tambahan gaya jika diperlukan */
-  </style>
-  
+}
+</script>
